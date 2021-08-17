c8281b6b53b68b45b82959990c1efa175ab2127e
Marius Tita <marius.tita81@gmail.com>
2021-08-17 14:23:16 +0300

    small improvements

--- R/mod_first_tab.R
+++ R/mod_first_tab.R
@@ -12,7 +12,10 @@ mod_first_tab_ui <- function(id){
   tagList(
   fileInput(inputId = ns("example_input"),label = "Input example file",accept = c(".xlsx",".xls"),
             buttonLabel = "Excel ony",placeholder = "no file uploaded"),
-  verbatimTextOutput(ns("value_file_input"))
+ 
+  verbatimTextOutput(ns("value_file_input")), 
+  hr(),
+  actionButton(inputId = ns("create_new_record"),label = "Click here to insert and save new record")
   )
 }
     
@@ -22,7 +25,21 @@ mod_first_tab_ui <- function(id){
 mod_first_tab_server <- function(input, output, session){
   ns <- session$ns
   
-  output$value_file_input <- renderPrint({input$example_input})
+  read_test <- read.csv(file = "R/reactivedata/test.csv")
+  
+  vals <- reactiveValues(read_test = read_test)
+  
+  output$value_file_input <- renderPrint({vals$read_test})
+  
+  observeEvent(input$create_new_record,{
+      vals$read_test <- dplyr::bind_rows(vals$read_test,data.frame(Col1=sample(x = 1:10,size = 1,replace = T), 
+                   Col2 = sample(LETTERS, size = 1, replace = T)))
+      
+      write.csv(x = vals$read_test, file="R/reactivedata/test.csv", row.names = F)
+      })
+ 
+      
+  
  
 }
     
